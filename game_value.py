9a4b5c8b8e2d0056db1b2bab19616d76da427fd5
chaseforlight <114924957+chaseforlight@users.noreply.github.com>
2022-10-08 15:13:25 +0800

    Create game_value.py

---
+++ game_value.py
@@ -0,0 +1,15 @@
+class GameValue:
+    window = None
+    my_tank = None
+    enemy_tank_list = []
+    enemy_tank_num = 10
+    my_bullet_list = []
+    max_bullet_num = 3
+    enemy_bullet_list = []
+    explore_list = []
+    game_over = False
+    lose_or_win = 0 #0 means lose,1 means win
+    wall_list = []
+    item_list = []
+    max_item_num = 3
+    button = None
